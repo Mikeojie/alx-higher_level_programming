5adf65d3fad6b3d84eee5d25b381dbf0c4b292d7
Okojie Micheal <okojiemicheal070@gmail.com>
2022-09-21 00:28:05 +0000

    python script

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    for i in number:
-	if number < 0:
-	    number = -1 * number
-	    return i = number % 10
-	else:
-	    return i = number % 10
+    if number < 0:
+        number = number * -1
+        print(number % 10, end="")
+        return number % 10
+    else:
+        print(number % 10, end="")
+        return number % 10
